157b7b953a2f7d36f6d55476d119c6134acdc4f4
Brandon White <brandonkwhite3@gmail.com>
2023-04-17 01:54:00 -0400

    About to start working on categories. Need to download slugify.

---
+++ models/categoryModel.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose'
+// Bringing slugify in to handle string formatting for category items.
+//  Need to download slugify still.
+
+// TODO 
+// /: go get slugify.
+// Download and install. 
+
+
+const categorySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    slug: {
+        type: String,
+        lowercase: true,
+    },
+})
+
+export default mongoose.model("categories", categorySchema)
